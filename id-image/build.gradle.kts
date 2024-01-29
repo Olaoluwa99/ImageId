6c2d6ff118a0929c563b3b6df59cd88b41019e22
David-IO <ifeoluwadavido1@gmail.com>
2024-01-29 23:34:58 +0100

    Openjdk update

--- id-image/build.gradle.kts
+++ id-image/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -58,7 +58,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.Olaoluwa99"
             artifactId = "id-image"
-            version = "1.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
